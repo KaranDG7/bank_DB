1e25e60f02a2d782effb02c74d7a56c07c6751af
Karan Gonbare <karangonbare@gmail.com>
2026-04-02 00:35:26 +0530

    fix pool variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 const express   = require('express');
-const Pool      = require('./db');
+const pool      = require('./db');
 const bcrypt    = require('bcrypt');
 const jwt       = require('jsonwebtoken');
 const cors      = require('cors');
